8196278552b597114fc28a69ea92e624645d9742
dominje <jericah.domingo@oocl.com>
2020-10-17 18:00:39 +0800

    [ParkingLot][jericah]test: created test cases that will allow service manager to ask parking boy to park/fetch car given that the parking boy is in the management list

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -1,6 +1,7 @@
 package com.oocl.cultivation;
 
 import com.oocl.cultivation.exception.NoParkingTicketException;
+import com.oocl.cultivation.exception.ParkingBoyNotInManagementListException;
 import com.oocl.cultivation.exception.ParkingLotFullException;
 import com.oocl.cultivation.exception.UnrecognizedParkingTicketException;
 import org.junit.jupiter.api.Test;
@@ -118,7 +119,7 @@ class ParkingBoyTest {
         ParkingTicket parkingTicket = parkingBoy.park(car);
 
         // when
-        Car carFetched = parkingBoy.fetchCar(parkingTicket);
+        parkingBoy.fetchCar(parkingTicket);
         //when
         UnrecognizedParkingTicketException thrown = assertThrows(
                 UnrecognizedParkingTicketException.class,
@@ -141,7 +142,7 @@ class ParkingBoyTest {
         parkingLots.add(parkingLot);
 
         ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
-        ParkingTicket parkingTicket1 = parkingBoy.park(car);
+        parkingBoy.park(car);
 
         //when
         ParkingLotFullException thrown = assertThrows(
@@ -165,7 +166,7 @@ class ParkingBoyTest {
         parkingLots.add(parkingLot);
         ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
         ParkingTicket parkingTicket = parkingBoy.park(car);
-        Car carFetched = parkingBoy.fetchCar(parkingTicket);
+        parkingBoy.fetchCar(parkingTicket);
 
         
         //when
@@ -184,7 +185,6 @@ class ParkingBoyTest {
     public void should_throw_an_exception_car_when_fetching_given_no_ticket(){
 
         //given
-        Car car = new Car();
         ParkingLot parkingLot = new ParkingLot();
         List<ParkingLot> parkingLots = new ArrayList<>();
         parkingLots.add(parkingLot);
@@ -210,7 +210,7 @@ class ParkingBoyTest {
         List<ParkingLot> parkingLots = new ArrayList<>();
         parkingLots.add(parkingLot);
         ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
-        ParkingTicket parkingTicket = parkingBoy.park(car);
+        parkingBoy.park(car);
         
         //when
         ParkingLotFullException thrown = assertThrows(
@@ -304,5 +304,65 @@ class ParkingBoyTest {
 
     }
 
+    @Test
+    public void should_park_car_and_give_ticket_when_car_arrives_given_car(){
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(parkingLot);
+        ServiceManager serviceManager = new ServiceManager(parkingLotList);
+        //when
+        ParkingTicket parkingTicket = serviceManager.park(car);
+        Car fetchedCar = serviceManager.fetchCar(parkingTicket);
+        
+        //then
+        assertSame(car, fetchedCar);
+    }
+    
+    @Test
+    public void should_specify_parking_boy_to_fetch_car_when_parking_boy_manages_lot_given_ticket() {
+        //given
+
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(parkingLot);
+        ParkingBoy parkingBoy1 = new ParkingBoy(parkingLotList);
+        ParkingBoy parkingBoy2 = new ParkingBoy(parkingLotList);
+        ServiceManager serviceManager = new ServiceManager(parkingLotList);
+        //when
+        serviceManager.addParkingBoyToManagementList(parkingBoy1);
+        serviceManager.addParkingBoyToManagementList(parkingBoy2);
+
+        ParkingTicket parkingTicket = serviceManager.askParkingBoyToParkCar(car, parkingBoy1);
+        Car fetchedCar = serviceManager.askParkingBoyToFetchCar(parkingTicket, parkingBoy2);
+
+        //then
+        assertSame(car, fetchedCar);
+        
+    }
+
+    @Test
+    public void should_throw_exception_when_asking_parking_boy_to_park_car_given_parking_boy_not_in_management_list() {
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot(1);
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(parkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+        ServiceManager serviceManager = new ServiceManager(parkingLotList);
+
+        //when
+        ParkingBoyNotInManagementListException thrown = assertThrows(
+                ParkingBoyNotInManagementListException.class,
+                () -> serviceManager.askParkingBoyToParkCar(car, parkingBoy),
+                "Expected fetch() to throw, but it didn't"
+        );
+
+        //then
+        assertTrue(thrown.getMessage().contains("Parking Boy not in Management List!"));
+
+    }
     
 }
