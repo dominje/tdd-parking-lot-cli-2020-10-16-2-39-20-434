34cc7db4895189f9d7d8d1b0edf522b9db752b96
dominje <jericah.domingo@oocl.com>
2020-10-17 15:06:15 +0800

    [ParkingLot][jericah]feature: added logic that will park on the larger available position rate given that there multiple parking lots in super smart parking boy

---
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -0,0 +1,33 @@
+package com.oocl.cultivation;
+
+import com.oocl.cultivation.exception.ParkingLotFullException;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+public class SuperSmartParkingBoy {
+    private List<ParkingLot> parkingLots;
+
+    public SuperSmartParkingBoy(List<ParkingLot> parkingLot) {
+        this.parkingLots = parkingLot;
+    }
+
+    // larger position rate = getPositionsAvailable()/getParkingLotCapacity()
+    public ParkingTicket park(Car car) {
+        ParkingLot parkingLot = parkingLots.stream()
+                .max(Comparator.comparing(ParkingLot::getLargestPositionRate)).orElse(null);
+        if (Objects.nonNull(parkingLot)) {
+            return parkingLot.park(car);
+        }
+        throw new ParkingLotFullException("Not enough position.");
+    }
+
+    public Car fetchCar(ParkingTicket parkingTicket) {
+        for (ParkingLot parkingLot : parkingLots) {
+            return parkingLot.validateParkingTicket(parkingTicket);
+        }
+        return null;
+    }
+
+}
