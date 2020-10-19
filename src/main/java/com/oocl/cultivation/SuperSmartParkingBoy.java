f9481e7b5c005a9d9386d1485a7092547d822562
dominje <jericah.domingo@oocl.com>
2020-10-19 18:43:37 +0800

    [ParkingLot][jericah]refactor: added override annotation for park method

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -13,7 +13,8 @@ public class SuperSmartParkingBoy extends ParkingBoy {
         super(parkingLot);
         this.parkingLots = parkingLot;
     }
-    
+
+    @Override
     public ParkingTicket park(Car car) {
         ParkingLot parkingLot = parkingLots.stream()
                 .max(Comparator.comparing(ParkingLot::getPositionRate)).orElse(null);
